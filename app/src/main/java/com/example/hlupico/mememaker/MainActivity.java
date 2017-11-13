a17953c860e36a6dfa4400eab1519b01c00eb36a
Molly King <mking@rmn.com>
2017-11-12 18:44:38 -0600

    Saves images to device folder called memes

--- app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
@@ -4,27 +4,35 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int REQUEST_CODE_SAVE = 44;
     private static final int REQUEST_CODE_CAMERA = 55;
     private static final int REQUEST_CODE_GALLERY = 66;
     private static final int REQUEST_CODE_TAKE_PHOTO = 77;
     private static final int REQUEST_CODE_CHOOSE_PHOTO = 88;
     private static final String[] CAMERA_PERMISSION = {Manifest.permission.CAMERA};
     private static final String[] GALLERY_PERMISSION = {Manifest.permission.READ_EXTERNAL_STORAGE};
+    private static final String[] SAVE_PERMISSION = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private ImageView backgroundImage;
 
@@ -72,6 +80,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 openGallery();
             } else {
                 Toast.makeText(this, "Need permissions to select a photo", Toast.LENGTH_LONG).show();
+            }
+        } else if (requestCode == REQUEST_CODE_SAVE) {
+            // did the user give us permission?
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                saveImage();
+            } else {
+                Toast.makeText(this, "Need permissions to save your creation", Toast.LENGTH_LONG).show();
             }
         }
     }
@@ -123,6 +138,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void saveImage() {
-
+        boolean storageWritePermissionGranted = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED;
+        if (!storageWritePermissionGranted) {
+            ActivityCompat.requestPermissions(this, SAVE_PERMISSION, REQUEST_CODE_SAVE);
+        } else {
+            View memeLayout = findViewById(R.id.meme_layout);
+            memeLayout.setDrawingCacheEnabled(true);
+            memeLayout.buildDrawingCache();
+            Bitmap full = memeLayout.getDrawingCache();
+            //todo: what are the other cases/should we worry about them?
+            if (Environment.getExternalStorageState().equalsIgnoreCase("mounted")) {
+                File imageFolder = new File(Environment.getExternalStorageDirectory(), "memes");
+                imageFolder.mkdirs();
+                FileOutputStream out = null;
+                File imageFile = new File(imageFolder, String.valueOf(System.currentTimeMillis()) + ".png");
+                try {
+                    out = new FileOutputStream(imageFile);
+                    full.compress(Bitmap.CompressFormat.PNG, 100, out);
+                    out.flush();
+                    out.close();
+                } catch (IOException e) {
+                    Log.e(TAG, "Error saving" + e.getLocalizedMessage());
+                } finally {
+                    out = null;
+                    MediaScannerConnection.scanFile(this, new String[] {imageFile.getAbsolutePath()}, null, null);
+                }
+            }
+            memeLayout.destroyDrawingCache();
+            memeLayout.setDrawingCacheEnabled(false);
+        }
     }
 }
