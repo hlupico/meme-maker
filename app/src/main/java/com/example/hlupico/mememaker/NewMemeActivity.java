722bc1a5e499381a46e0cb691d345890f200e56b
Molly King <mking@rmn.com>
2017-11-29 19:27:17 -0600

    initial setup

--- app/src/main/java/com/example/hlupico/mememaker/NewMemeActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/NewMemeActivity.java
@@ -1,8 +1,12 @@
 package com.example.hlupico.mememaker;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,6 +16,14 @@ import android.view.View;
  */
 
 public class NewMemeActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_CAMERA = 55;
+    private static final int REQUEST_CODE_GALLERY = 66;
+    private static final int REQUEST_CODE_TAKE_PHOTO = 77;
+    private static final int REQUEST_CODE_CHOOSE_PHOTO = 88;
+    private static final String[] CAMERA_PERMISSION = {Manifest.permission.CAMERA};
+    private static final String[] GALLERY_PERMISSION = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +38,38 @@ public class NewMemeActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.gallery_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        findViewById(R.id.camera_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+        // TODO: Check for permission to use camera
+
+        //TODO: when permission is granted, open camera
+    }
+
+    private void openGallery() {
+        //TODO: Check for permission to view gallery
+
+        //TODO: when permission is granted, open gallery
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //TODO: take action if permissions are granted
     }
 
     @Override
