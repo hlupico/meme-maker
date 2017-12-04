54fcfebd824b491dc396fc1d669b61f84801fc6b
Molly King <mking@rmn.com>
2017-12-03 19:18:19 -0600

    transmit image and text to preview activity (passing images is really complicated)

--- app/src/main/java/com/example/hlupico/mememaker/NewMemeActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/NewMemeActivity.java
@@ -6,17 +6,23 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by melder on 11/17/17.
@@ -30,6 +36,11 @@ public class NewMemeActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_CHOOSE_PHOTO = 88;
     private static final String[] CAMERA_PERMISSION = {Manifest.permission.CAMERA};
     private static final String[] GALLERY_PERMISSION = {Manifest.permission.READ_EXTERNAL_STORAGE};
+    private Bitmap memeImage;
+    private Uri imageUri;
+    private Uri tempImageUri;
+    private EditText topText;
+    private EditText bottomText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,11 +48,18 @@ public class NewMemeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_meme);
         setTitle("Make New Meme");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        topText = (EditText)findViewById(R.id.top_text_input);
+        bottomText = (EditText)findViewById(R.id.bottom_text_input);
 
         findViewById(R.id.preview_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(NewMemeActivity.this, PreviewMemeActivity.class);
+                //Send the text and photo
+
+                intent.putExtra("image", imageUri.toString());
+                intent.putExtra("topText", topText.getText().toString());
+                intent.putExtra("bottomText", bottomText.getText().toString());
                 startActivity(intent);
             }
         });
@@ -67,7 +85,19 @@ public class NewMemeActivity extends AppCompatActivity {
         if (cameraPermissionGranted == false) {
             ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, REQUEST_CODE_CAMERA);
         } else {
+            String imageFileName = "meme_img" + DateFormat.format("yyyy-MM-dd_hhmmss", new Date()).toString()+".jpg";
+            File newfile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES).getPath(), imageFileName);
+            try {
+                newfile.createNewFile();
+            } catch (IOException e) {}
+
+            tempImageUri = FileProvider.getUriForFile(
+                    this,
+                    getApplicationContext()
+                            .getPackageName() + ".provider", newfile);
+
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempImageUri);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) { // device has a camera app
                 startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
             }
@@ -116,14 +146,18 @@ public class NewMemeActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE_TAKE_PHOTO && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            setThumbnail(imageBitmap);
-        } else if (requestCode == REQUEST_CODE_CHOOSE_PHOTO && resultCode == RESULT_OK) {
-            Uri uri = data.getData();
+            imageUri = tempImageUri;
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                setThumbnail(bitmap);
+                memeImage = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                setThumbnail(memeImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == REQUEST_CODE_CHOOSE_PHOTO && resultCode == RESULT_OK) {
+            imageUri = data.getData();
+            try {
+                memeImage = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                setThumbnail(memeImage);
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- app/src/main/java/com/example/hlupico/mememaker/PreviewMemeActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/PreviewMemeActivity.java
@@ -1,10 +1,17 @@
 package com.example.hlupico.mememaker;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 /**
  * Created by melder on 11/17/17.
@@ -17,6 +24,22 @@ public class PreviewMemeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preview_meme);
         setTitle("Preview");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Bundle extras = getIntent().getExtras();
+        String topText = extras.getString("topText");
+        String bottomText = extras.getString("bottomText");
+        Uri imageUri = Uri.parse(extras.getString("image"));
+
+        ImageView background = (ImageView)findViewById(R.id.background_image);
+        try {
+            Bitmap backgroundImage = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+            background.setImageBitmap(backgroundImage);
+        } catch (IOException e) {}
+
+        TextView topTextView = (TextView)findViewById(R.id.top_text);
+        topTextView.setText(topText);
+        TextView bottomTextView = (TextView)findViewById(R.id.bottom_text);
+        bottomTextView.setText(bottomText);
+
 
         findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
             @Override
