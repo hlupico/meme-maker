82cf03bdf5ffba29fbd61ae4ff913ac207ab4476
Michael Speed Elder <melder@rmn.com>
2017-12-07 16:09:32 -0600

    Finish the home page screen!

--- app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
+++ app/src/main/java/com/example/hlupico/mememaker/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.hlupico.mememaker;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,14 +9,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setElevation(0f);  /** Step 1 - remove automatic action bar shadow */
-
-        /** Step 2 - start New Meme Activity when button is clicked on */
-        findViewById(R.id.new_meme_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NewMemeActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
